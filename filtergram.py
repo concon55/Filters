b6be3b015681f9e6ae530c816bbee61422a9cb61
concon55 <clg469@nyu.edu>
2019-07-15 16:04:00 -0400

    Update filtergram.py

--- filtergram.py
+++ filtergram.py
@@ -3,9 +3,9 @@ import filters
 def main():
     userInput = input("Enter file name: ")
     im = filters.load_img(userInput)
-    # newImage = filters.obamicon(im)
-    filters.show_img(im)
-    filters.save_img(im, "newPic.jpg")
+    newImage = filters.obamicon(im)
+    filters.show_img(newImage)
+    filters.save_img(newImage, "newPic.jpg")
 
 if __name__ == "__main__":
   main()
